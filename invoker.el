d09e960cccb5dca4a27594ec24f6579386e2e9ac
Hemant Kumar <gethemant@gmail.com>
2013-12-17 10:24:46 +0530

    Add a command for listing processes

--- invoker.el
+++ invoker.el
@@ -10,4 +10,16 @@
   (interactive "p\nMCommand to stop: ")
   (start-process "invoker" "*invoker*" "invoker" "remove" command))
 
+(defun invoker-list ()
+  (interactive)
+  (let (
+        (invoker-list-buffer (get-buffer-create "*invoker list*"))
+        )
+    (switch-to-buffer invoker-list-buffer)
+    (call-process "invoker" nil invoker-list-buffer "invoker" "list")
+    (ansi-color-apply-on-region (point-min) (point-max))
+    (beginning-of-buffer)
+    ))
+
+
 (provide 'invoker)
